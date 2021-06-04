501d26e6082f0e718d95738cfb2e46e3dafa5b37
Nakata4 <73024774+Nakata4@users.noreply.github.com>
2021-06-04 21:10:33 +0300

    bonus9

---
+++ CarsOnline/admin/booked.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Car Shop</title>
+</head>
+
+<body>
+    
+     <h1> Car is Booked </h1>
+</body>
+</html>
